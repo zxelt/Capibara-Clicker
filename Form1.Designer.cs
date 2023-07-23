5c345fb67c9337ff5825932f78ab0b00255c2e0c
Axel <108530057+zxelt@users.noreply.github.com>
2023-07-23 15:16:58 +0200

    First

---
+++ Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace Capibara_Clicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Capibara = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Lvl = new System.Windows.Forms.Label();
+            this.Newlvl = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Capibara)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Capibara
+            // 
+            this.Capibara.ErrorImage = ((System.Drawing.Image)(resources.GetObject("Capibara.ErrorImage")));
+            this.Capibara.Image = ((System.Drawing.Image)(resources.GetObject("Capibara.Image")));
+            this.Capibara.Location = new System.Drawing.Point(313, 74);
+            this.Capibara.Name = "Capibara";
+            this.Capibara.Size = new System.Drawing.Size(256, 260);
+            this.Capibara.TabIndex = 0;
+            this.Capibara.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Cascadia Code ExtraLight", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(327, 373);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(202, 65);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "CLICK !";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cascadia Code ExtraLight", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(391, -3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 63);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "0";
+            // 
+            // Lvl
+            // 
+            this.Lvl.AutoSize = true;
+            this.Lvl.Font = new System.Drawing.Font("Cascadia Code SemiBold", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lvl.ForeColor = System.Drawing.Color.White;
+            this.Lvl.Location = new System.Drawing.Point(569, 381);
+            this.Lvl.Name = "Lvl";
+            this.Lvl.Size = new System.Drawing.Size(154, 49);
+            this.Lvl.TabIndex = 3;
+            this.Lvl.Text = "Level:";
+            // 
+            // Newlvl
+            // 
+            this.Newlvl.AutoSize = true;
+            this.Newlvl.Font = new System.Drawing.Font("Cascadia Code ExtraLight", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Newlvl.ForeColor = System.Drawing.Color.Yellow;
+            this.Newlvl.Location = new System.Drawing.Point(8, 411);
+            this.Newlvl.Name = "Newlvl";
+            this.Newlvl.Size = new System.Drawing.Size(179, 32);
+            this.Newlvl.TabIndex = 4;
+            this.Newlvl.Text = "New Level !";
+            this.Newlvl.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cascadia Code SemiLight", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(5, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 49);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Shop:";
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Cascadia Code", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(12, 76);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(167, 41);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "THUG CAPIBARA";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Cascadia Code SemiBold", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(185, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 49);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "100";
+            // 
+            // button3
+            // 
+            this.button3.AutoSize = true;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Cascadia Code", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(692, 9);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(96, 41);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "RESET";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.Newlvl);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Location = new System.Drawing.Point(0, -2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(278, 452);
+            this.panel1.TabIndex = 9;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 150;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.Lvl);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Capibara);
+            this.MaximumSize = new System.Drawing.Size(816, 489);
+            this.MinimumSize = new System.Drawing.Size(816, 489);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Capibara Clicker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Capibara)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Capibara;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Lvl;
+        private System.Windows.Forms.Label Newlvl;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+using System.Windows.Forms;
+
+namespace Capibara_Clicker
+{
+    public partial class Form1 : Form
+    {
+        private int point = 0;
+        private int level = 0;
+        private int Necessarypoints;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void lvlplus()
+        {
+            Newlvl.Visible = true;
+            level++;
+            string lvl = level.ToString();
+            Lvl.Text = $"Level: {lvl}";
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            point++;
+            label1.Text = point.ToString();
+            if (point == 10)
+            {
+                lvlplus();
+            }
+            else if (point == 100)
+            {
+                lvlplus();
+            }
+            else if (point == 1000)
+            {
+                lvlplus();
+            }
+            else if (point == 10000)
+            {
+                lvlplus();
+            }
+            else if (point == 100000)
+            {
+                lvlplus();
+            }
+            else if (point == 1000000)
+            {
+                Newlvl.Text = "Stop cheating";
+                Newlvl.ForeColor = Color.White;
+                MessageBox.Show ("Cheat is bad");
+            }
+            else
+            {
+                Newlvl.Visible = false;
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string lvl = level.ToString();
+            Lvl.Text = $"Level {lvl}";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Necessarypoints = 100;
+            buy();
+
+            label1.Text = point.ToString();
+        }
+
+        private void buy()
+        {
+            if (point >= Necessarypoints)
+            {
+                MessageBox.Show("Successful purchase !");
+                point -= Necessarypoints;
+                Capibara.Image = Capibara.ErrorImage;
+            }
+            else
+            {
+
+                Necessarypoints -= point;
+                MessageBox.Show($"You don't have enough points you are {Necessarypoints} points short");
+            }
+        }
+        private void buy1()
+        {
+            if (point >= Necessarypoints)
+            {
+                MessageBox.Show("Successful purchase !");
+                point -= Necessarypoints;
+                timer1.Enabled = true;
+            }
+            else
+            {
+
+                Necessarypoints -= point;
+                MessageBox.Show($"You don't have enough points you are {Necessarypoints} points short");
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            point -= point;
+            label1.Text = point.ToString();
+            Capibara.Image = Capibara.Image;
+            MessageBox.Show("Reset successfully");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Necessarypoints = 10;
+            buy1();
+            label1.Text = point.ToString();
+        }
+    }
+}
